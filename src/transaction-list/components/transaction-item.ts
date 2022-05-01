f1148f73091dbe0976821c60cf75a597219eee75
Pavel Pleshko <pavel.pleshko@sbgenomics.com>
2022-05-01 19:08:07 +0200

    refactor(transactions): extract classes into stylesheet

--- src/transaction-list/components/transaction-item.ts
+++ src/transaction-list/components/transaction-item.ts
@@ -19,23 +19,23 @@ export class TransactionItem extends LitElement {
     render (): ReturnType<typeof html> {
         return html`
             <ul>
-                <li class="flex justify-between">
-                    <span class="mr-3">Date</span><span>${ new Date(+this.transaction.timeStamp * 1000).toUTCString() }</span>
+                <li>
+                    <span>Date</span><span>${ new Date(+this.transaction.timeStamp * 1000).toUTCString() }</span>
                 </li>
-                <li class="flex justify-between">
-                    <span class="mr-3">From</span><span>${ this.transaction.from }</span>
+                <li>
+                    <span>From</span><span>${ this.transaction.from }</span>
                 </li>
-                <li class="flex justify-between">
-                    <span class="mr-3">To</span><span>${ this.transaction.to }</span>
+                <li>
+                    <span>To</span><span>${ this.transaction.to }</span>
                 </li>
-                 <li class="flex justify-between">
-                    <span class="mr-3">Amount</span><span>${ formatCurrency(this.transaction.value, Currency.ETH) } <b>ETH</b></span>
+                 <li>
+                    <span>Amount</span><span>${ formatCurrency(this.transaction.value, Currency.ETH) } <b>ETH</b></span>
                 </li>
-                 <li class="flex justify-between">
-                    <span class="mr-3">Fee</span><span>${ formatCurrency(this.transaction.gasUsed * this.transaction.gasPrice, Currency.ETH, 10) } <b>ETH</b></span>
+                 <li>
+                    <span>Fee</span><span>${ formatCurrency(this.transaction.gasUsed * this.transaction.gasPrice, Currency.ETH, 10) } <b>ETH</b></span>
                 </li>
-                <li class="flex justify-between">
-                    <span class="mr-3">Etherscan</span><span>${ trimAddress(this.transaction.hash) }</span>
+                <li>
+                    <span>Etherscan</span><span>${ trimAddress(this.transaction.hash) }</span>
                 </li>
             </ul>
        `;
